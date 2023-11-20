3a572d565fa73f1b181609b78ef2788baeefbfd3
abila <abila@Bilal-Ahmad>
2023-11-20 09:59:45 -0500

    This is my first commit

---
+++ src/projectforTestersGit/TestClass.java
@@ -0,0 +1,24 @@
+package projectforTestersGit;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a project for get");
+		System.out.println("Put it in staging");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
